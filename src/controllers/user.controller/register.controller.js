1c13f8e2dac8c3175e84126ed519bb48ba55bc30
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-11-04 15:36:53 +0100

    UPDATE google oauth2

--- src/controllers/user.controller/register.controller.js
+++ src/controllers/user.controller/register.controller.js
@@ -1,11 +1,13 @@
 const bcrypt = require('bcrypt');
 const User = require('../../models/users-model/user.model');
+const emailWelcome = require('./mails/emailWelcome');
 
 const RegisterController = async (req, res, next) => {
   const email = req.body.email.toLowerCase();
   try {
     const createUser = new User({ ...req.body, email });
-    await createUser.save();
+    await createUser.save();    
+    await emailWelcome(createUser)
     const user = await User.findById({ _id: createUser._id });
     return res
       .status(201)
