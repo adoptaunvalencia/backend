6510551eb5d6dd4c5343388a8cd67c1a98e2261f
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-12-08 00:12:57 +0100

    UPDATE createContactEmail

--- src/controllers/contactEmail.controller/createContactEmail.controller.js
+++ src/controllers/contactEmail.controller/createContactEmail.controller.js
@@ -1,12 +1,17 @@
+const AssistanceOffer = require('../../models/assistance-offer-model/assistanceOffer.model');
 const ContactEmail = require('../../models/contact-email-model/contactEmail.model');
 const User = require('../../models/users-model/user.model');
 const emailContact = require('./emails/emailContact');
 
 const createContactEmail = async (req, res, next) => {
+  const userSend = req.user;
+  const { subject, body } = req.body.formData;
+  const { offer } = req.body;
   try {
-    const { subject, body } = req.body.formData;
-    const userSend = req.user;
     const userReceive = await User.findById({ _id: req.body.offer.userId._id });
+    const updateOffer = await AssistanceOffer.findByIdAndUpdate(offer._id, {
+      $set: { status: false },
+    });
 
     const createContactEmail = new ContactEmail({
       userSendId: userSend._id,
